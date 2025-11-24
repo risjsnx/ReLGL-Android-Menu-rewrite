b9cc5b922f6f8c148adfe942f7c8d5add1e6ad7a
Parsa307 <79400634+Parsa307@users.noreply.github.com>
2025-11-24 19:09:42 +0330

    Patch original get_device_api_level_inlines.h

--- app/src/main/jni/Menu/get_device_api_level_inlines.h
+++ app/src/main/jni/Menu/get_device_api_level_inlines.h
@@ -25,23 +25,16 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-
 #pragma once
-
-
 #include <sys/cdefs.h>
-
 __BEGIN_DECLS
-
 // Avoid circular dependencies since this is exposed from <sys/cdefs.h>.
-int __system_property_get(const char* __name, char* __value);
-int atoi(const char* __s) __attribute_pure__;
-
+int __system_property_get(const char* _Nonnull __name, char*  _Nonnull __value);
+int atoi(const char* _Nonnull __s) __attribute_pure__;
 int api_level() {
   char value[92] = { 0 };
   if (__system_property_get("ro.build.version.sdk", value) < 1) return -1;
   int api_level = atoi(value);
   return (api_level > 0) ? api_level : -1;
 }
-
 __END_DECLS
